2586cfe4b6b165b5fa4269dcc9ace8baf532d5ec
Peter Mai <pm123633@gmail.com>
2021-08-15 21:04:12 -0500

    Add express-session

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,4 @@
 export const port = process.env.PORT || 5000;
 export const environment = process.env.NODE_ENV || 'development';
+export const secret = process.env.SECRET || 'some secret';
+export const maxAge = +(process.env.MAX_AGE || 604800);
